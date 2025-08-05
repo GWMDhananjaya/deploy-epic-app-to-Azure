d8c9a4078639fcdf3cbfa0a3bcb5fccf9fbd52bb
Dhananjaya Rupasinghe <106276257+GWMDhananjaya@users.noreply.github.com>
2025-08-06 05:15:57 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,10 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import flowbiteReact from "flowbite-react/plugin/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react(), flowbiteReact()],
   build: {
-    outDir: 'dist',
+    outDir: "dist",
   },
+  base: "/", // ✅ Add this line
 });
